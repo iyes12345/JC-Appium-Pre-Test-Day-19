8fab164800887eda6baaffb291772bb7dcd34b61
Mohammad Zakaria <zakariayus10@gmail.com>
2022-10-23 21:49:04 +0700

    Pre Test Day 19 commit

---
+++ src/main/java/com/juaracoding/appium/pages/Calculator.java
@@ -0,0 +1,73 @@
+package com.juaracoding.appium.pages;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.By;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.support.PageFactory;
+public class Calculator {
+
+    public AndroidDriver<MobileElement> driver;
+
+    public Calculator(AndroidDriver<MobileElement> driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/digit_1")
+    private MobileElement btnSatu;
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/digit_2")
+    private MobileElement btnDua;
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/digit_3")
+    private MobileElement btnTiga;
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/digit_4")
+    private MobileElement btnEmpat;
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/op_add")
+    private MobileElement btnTambah;
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/op_mul")
+    private MobileElement btnKali;
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/eq")
+    private MobileElement btnSamaDengan;
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/result_final")
+    private MobileElement hasil;
+
+
+
+    //Method
+    public void calcAdd() {
+        btnSatu.click();
+        System.out.println("Button number 1 clicked");
+        btnTambah.click();
+        System.out.println("Button plus clicked");
+        btnDua.click();
+        System.out.println("Button number 2 clicked");
+        btnSamaDengan.click();
+        System.out.println("Button result clicked");
+    }
+
+    public void calcMultiplication() {
+        btnTiga.click();
+        System.out.println("Button number 2 clicked");
+        btnKali.click();
+        System.out.println("Button Multiplication clicked");
+        btnEmpat.click();
+        System.out.println("Button number 4 clicked");
+        btnSamaDengan.click();
+        System.out.println("Button result clicked");
+    }
+
+    //
+    public String getTxtResult() {
+        return hasil.getText();
+    }
+
+}
